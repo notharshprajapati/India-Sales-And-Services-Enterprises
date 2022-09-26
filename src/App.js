90feb3a04daa7dacd8e2b91c81009c14914800da
notharshprajapati <prajapatiharsh0243@gmail.com>
2022-09-26 15:42:29 +0530

    fixed ?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { motion } from "framer-motion";
 
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -8,45 +7,20 @@ import Contact from "./pages/contact";
 import Services from "./pages/services";
 import NotFound from "./pages/NotFound";
 import SharedLayout from "./pages/SharedLayout";
-import Loading from "./UI/Loading";
 
 function App() {
-  const [name, setName] = useState(true);
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setName(false);
-    }, 0);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  });
-
   return (
-    <React.Fragment>
-      {name && (
-        <motion.div
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 0 }}
-          transition={{ delay: 1, default: { duration: 1.5 } }}
-        >
-          <Loading />
-        </motion.div>
-      )}
-      {!name && (
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<SharedLayout />}>
-              <Route index element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="services" element={<Services />} />
-              <Route path="contact" element={<Contact />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      )}
-    </React.Fragment>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { AnimatePresence } from "framer-motion";
 
-ReactDOM.render(
-  <AnimatePresence>
-    <App />
-  </AnimatePresence>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
